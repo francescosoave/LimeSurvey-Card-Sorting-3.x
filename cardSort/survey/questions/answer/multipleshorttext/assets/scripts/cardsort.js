dd542932985950d533d9a91f54f7101d65aa29e7
Francesco Soave <26126452+francescosoave@users.noreply.github.com>
2020-03-31 19:25:03 +0100

    add default placeholders css class

--- cardSort/survey/questions/answer/multipleshorttext/assets/scripts/cardsort.js
+++ cardSort/survey/questions/answer/multipleshorttext/assets/scripts/cardsort.js
@@ -1,31 +1,39 @@
 
 $(document).on('ready pjax:scriptcomplete',function(){
-	
+
 	$('.card-sort-outer-wrapper').each(function(i) {
 
 		// Define some stuff...
 		var thisQuestion = $(this).closest('.multiple-short-txt');
 		var qID = $(thisQuestion).attr('id').replace(/question/, '');
-		
+
 		if($('#question'+qID+' .card.draggable').length == 0) {
-		
-			$('#question'+qID).addClass('card-sort-question');	
+
+			$('#question'+qID).addClass('card-sort-question');
 			$('body').addClass('with-card-sort-question');
-			
+
 			// Insert the "cards"
 			$('#question'+qID+' .answer-item:not(.ls-irrelevant)').each(function(i) {
 				var thisSGQA = $(this).attr('id').replace(/javatbd/, '');
 				var thisCode = $(this).attr('id').split('X'+qID)[1];
 				var thisHTML = $('label', this).html();
-				$('#question'+qID+' div.items-start').append('<div class="card draggable" data-sgqa="'+thisSGQA+'" data-code="'+thisCode+'">\
+
+				// search for default values
+				if(thisCode.includes("Def")){
+					$('#question'+qID+' div.items-start').append('<div class="defaultOption card draggable" data-sgqa="'+thisSGQA+'" data-code="'+thisCode+'">\
+													'+thisHTML+'\
+												</div>');
+				}else{
+					$('#question'+qID+' div.items-start').append('<div class="card draggable" data-sgqa="'+thisSGQA+'" data-code="'+thisCode+'">\
 												'+thisHTML+'\
 											</div>');
-			});			
-		 
+				}
+			});
+
 			// Make the "cards" draggable
-			$('#question'+qID+' .draggable').draggable({ 
-				revert: "invalid", 
-				zIndex: 2700, 
+			$('#question'+qID+' .draggable').draggable({
+				revert: "invalid",
+				zIndex: 2700,
 				helper: 'original',
 				start: function( event, ui ) {
 					$(ui.helper).addClass('ui-draggable-helper');
@@ -33,20 +41,20 @@ $(document).on('ready pjax:scriptcomplete',function(){
 				stop: function( event, ui ) {
 				}
 			});
-		 
+
 			// Set the targets for the draggables
-			$('#question'+qID+' .droppable.items-start').droppable({ 
-				hoverClass: 'target-hover', 
-				accept: '.draggable.moved' 
+			$('#question'+qID+' .droppable.items-start').droppable({
+				hoverClass: 'target-hover',
+				accept: '.draggable.moved'
 			});
-			$('#question'+qID+' .droppable.items-end').droppable({ 
-				hoverClass: 'target-hover', 
-				accept: '.draggable' 
+			$('#question'+qID+' .droppable.items-end').droppable({
+				hoverClass: 'target-hover',
+				accept: '.draggable'
 			});
-		 
+
 			// After dropped
 			$('#question'+qID+' .droppable').bind('drop', function(event, ui) {
-		 
+
 				var newDraggable = $(ui.draggable).clone();
 				$(newDraggable).appendTo(this);
 				$(ui.draggable).remove();
@@ -58,14 +66,14 @@ $(document).on('ready pjax:scriptcomplete',function(){
 				}
 				$(newDraggable).removeClass('ui-draggable-helper ui-draggable-dragging').css({
 					'z-index':'',
-					'top':'auto', 
+					'top':'auto',
 					'left':'auto'
 				});
-		 
+
 				// Now, make this new clone draggable
-				$(newDraggable).draggable({ 
-					revert: "invalid", 
-					zIndex: 2700, 
+				$(newDraggable).draggable({
+					revert: "invalid",
+					zIndex: 2700,
 					helper: 'original',
 					start: function( event, ui ) {
 						$(ui.helper).addClass('ui-draggable-helper');
@@ -73,7 +81,7 @@ $(document).on('ready pjax:scriptcomplete',function(){
 					stop: function( event, ui ) {
 					}
 				});
-				
+
 				// Load the hidden inputs
 				$('#question'+qID+' .question-item textarea, #question'+qID+' .question-item input[type="text"]').val('');
 				$('#question'+qID+' .droppable.items-end .card').each(function(i) {
@@ -83,7 +91,7 @@ $(document).on('ready pjax:scriptcomplete',function(){
 				});
 				$('#question'+qID+' .question-item textarea, #question'+qID+' .question-item input[type="text"]').trigger('keyup');
 			});
-		 
+
 			// Initial "card" positions
 			$('#question'+qID+' .question-item textarea, #question'+qID+' .question-item input[type="text"]').each(function(i) {
 				if($.trim($(this).val()) != '') {
@@ -92,4 +100,5 @@ $(document).on('ready pjax:scriptcomplete',function(){
 			});
 		}
 	});
+
 });
