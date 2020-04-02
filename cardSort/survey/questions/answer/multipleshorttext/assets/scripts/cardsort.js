a109e04a0c0f8294b5be8be54fe6a6dca6294e94
Francesco Soave <26126452+francescosoave@users.noreply.github.com>
2020-04-02 10:14:28 +0100

    add scrolling

--- cardSort/survey/questions/answer/multipleshorttext/assets/scripts/cardsort.js
+++ cardSort/survey/questions/answer/multipleshorttext/assets/scripts/cardsort.js
@@ -101,4 +101,21 @@ $(document).on('ready pjax:scriptcomplete',function(){
 		}
 	});
 
+	//Scrolling option:
+	
+	// make sure position is relative in css
+	var element = $('.card-sort-question .items-start'),
+    originalY = element.offset().top;
+    // Space between element and top of screen (when scrolling)
+    var topMargin = 100;
+    $(window).on('scroll', function(event) {
+        var scrollTop = $(window).scrollTop();
+
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 300);
+    });
+
 });
